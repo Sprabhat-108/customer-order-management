9c86de8b8af5490b44073ca8c931713120903e97
prabhat <prabhat.kumar39@gmail.com>
2025-11-30 22:03:27 +0530

    Initial commit

---
+++ src/main/java/com/prabhat/customerorder/management/CustomerOrderManagementApplication.java
@@ -0,0 +1,13 @@
+package com.prabhat.customerorder.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerOrderManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerOrderManagementApplication.class, args);
+	}
+
+}
